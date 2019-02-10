376d0ce6e7839e10828c47c86d68e12ee6fad0b2
sanket <sanketm221995@gmail.com>
2019-02-10 13:17:41 +0530

    Initial commit

---
+++ indec.js
@@ -0,0 +1,84 @@
+$('#navigation a').on('click', function(e) {
+  e.preventDefault();
+  var hash = this.hash;
+  $('html, body').animate({
+    scrollTop: $(this.hash).offset().top
+  }, 1000);
+});
+
+
+$('.toggler, .nav-content a:not(#dropdown-link)').on('click', function(){
+  $('.toggler').toggleClass('change');
+  $('.nav-content').slideToggle();
+  $('#dropdown-menu').slideUp();
+  $('.menu-overlay').toggle();
+});
+
+$('.nav-content .dropdown').on('click', function(){
+  $('#dropdown-menu').slideToggle();
+});
+
+$('.menu-overlay').on('click', function(){
+  $('.toggler').removeClass('change');
+  $('.nav-content').slideUp();
+  $('#dropdown-menu').slideUp();
+  $('.menu-overlay').hide();
+});
+
+$("#contact input, #contact textarea").on('focusout', function(){
+
+  var text_val = $(this).val();
+  if (text_val === "") {
+    $(this).removeClass('has-value');
+  } else {
+    $(this).addClass('has-value');
+  }
+
+});
+window.onload = function () {
+    var title = ["This is sanket Mokashi","Software Developer"];
+var subtitle = ["Hipster" , "Hacker" , "Hustler"];
+var options = {
+  strings: title,
+  typeSpeed: 40,
+    stringsElement: null,
+		// typing speed
+		// time before typing starts
+		startDelay: 0,
+		// backspacing speed
+		backSpeed: 20,
+		// time before backspacing
+		backDelay: 500,
+		// loop
+		loop: true,
+		// false = infinite
+		loopCount: 5,
+		// show cursor
+		showCursor: false,
+		// character for cursor
+		cursorChar: "|",
+		// attribute to type (null == text)
+		attr: null,
+		// either html or text
+		contentType: 'html',
+		// call when done callback function
+		callback: function() {},
+		// starting callback function before each string
+		preStringTyped: function() {},
+		//callback for every typed string
+		onStringTyped: function() {},
+		// callback for reset
+		resetCallback: function() {}
+};
+
+var typed = new Typed(".Title_Typed", options,
+    );
+options.strings = subtitle ;
+options.startDelay = 2000;
+options.loop = false;
+options.onComplete = function () {
+    $('.subTyped').value = "Hustler  Hacker  Hipster";
+};
+var sub_typed = new Typed(".subTyped" , options);
+
+};
